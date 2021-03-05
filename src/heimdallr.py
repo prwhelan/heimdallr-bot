9770c53a49975778735d89aecbdabacf16c32d0e
Patty Cakes <pwhelan717@gmail.com>
2021-03-05 15:07:25 -0500

    Add command to start the server
    Since the server can now auto-shutoff, add a command to start the server
    for when I'm not around.
    
    Also, the bot only responds to commands from the "valheim-bot" channel,
    giving people a way to ignore this chat if need be.

--- src/heimdallr.py
+++ src/heimdallr.py
@@ -1,19 +1,34 @@
 import discord
 import boto3
 
-version = "1.0.0"
+version = "1.1.1"
 client = discord.Client()
 ssm = boto3.client('ssm')
+ec2 = boto3.client('ec2')
+
+# AWS part of the bot
 
 # Find our EC2 instance running the valheim server
 # Figure out a better way to do this, probably another file
-server = boto3.client('ec2').describe_instances(
-        Filters=[
-            {
-                'Name': 'tag:heimdallr',
-                'Values': [''],
-            },
-        ])['Reservations'][0]['Instances'][0]
+def server():
+    return ec2.describe_instances(
+            Filters=[
+                {
+                    'Name': 'tag:heimdallr',
+                    'Values': [''],
+                },
+            ])['Reservations'][0]['Instances'][0]
+
+def startServer():
+    currentServer = server()
+    if(currentServer['State']['Name'] == 'stopped'):
+        instanceId = currentServer['InstanceId'];
+        ec2.start_instances(InstanceIds=[instanceId], DryRun=False)
+        return "Starting Valheim Server, this is a dry run and not actually doing anything yet."
+    else:
+        return "Valheim Server is currently {} and can only start from stopped.".format(currentServer['State']['Name'])
+
+# Discord part of the bot
 
 @client.event
 async def on_ready():
@@ -24,6 +39,10 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    # Stop spamming everyone
+    if message.channel.name.lower() != "valheim-bot":
+        return
+
     if message.content.lower() == "-v" or message.content.lower() == "version":
         await message.channel.send(version)
 
@@ -31,18 +50,21 @@ async def on_message(message):
         await message.channel.send(("Available commands:\n"
                                     "-server ip\n"
                                     "-server port\n"
-                                    "-server status\n"));
+                                    "-server status\n"
+                                    "-server start\n"));
 
     if message.content.lower() == "-server ip":
-        await message.channel.send(server['PublicIpAddress'])
+        await message.channel.send(server()['PublicIpAddress'])
 
     if message.content.lower() == "-server port":
         await message.channel.send("For the Valheim Client: 2456")
         await message.channel.send("For the Steam Client: 2457")
 
     if message.content.lower() == "-server status":
-        await message.channel.send(server['State']['Name'])
+        await message.channel.send(server()['State']['Name'])
 
+    if message.content.lower() == "-server start":
+        await message.channel.send(startServer())
 
 discordToken = ssm.get_parameter(Name='heimdallr-token', WithDecryption=True)['Parameter']['Value']
 client.run(discordToken)
